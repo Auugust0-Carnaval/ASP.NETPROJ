847a1d6c298cf9bc75dcfce1fad5af5425f30866
Augusto <augustooliveixa@gmail.com>
2023-05-02 20:56:23 -0300

    colocando Anotaçoes nas Props: price e realeseDate

--- models/Movie.cs
+++ models/Movie.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PAGESNET.models
 {
@@ -11,9 +12,12 @@ namespace PAGESNET.models
         public int Id { get; set; }
         public string? Title { get; set; } 
 
+        [Display(Name = "Realese Date")] //a exibiçao do campo (propriedade) ao invez de realeseDate fica : Realese Date
         [DataType(DataType.Date)]
         public DateTime RealeseDate { get; set; } // atributo do tipo date
         public string? Genre { get; set; } // ? = propriedade anulavel, pode ser atribuido valor nulo
+
+        [Column(TypeName = "decimal(18,2)")] //Adiciona o tipo de coluna na base de dados usando EF, com 18 digitos e duas casa descimais
         public decimal Price { get; set; }
     }
 }
