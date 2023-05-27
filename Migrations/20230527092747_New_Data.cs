26041d1f45ff968914aa09b9eacaed3a152df802
Augusto <augustooliveixa@gmail.com>
2023-05-27 17:06:01 -0300

    subindo arquivos de configuracao e migracao EF

---
+++ Migrations/20230527092747_New_Data.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PAGESNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class New_Data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
